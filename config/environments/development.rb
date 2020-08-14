63b7f03a1026ba382592ccb54a454d296dddf34d
AReallyTallDwarf <aaronverjans1@hotmail.com>
2020-08-14 16:35:43 +0200

    Add image uploading with paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,3 +65,6 @@ end
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
+
+# needed for paperclip
+Paperclip.options[:command_path] = "usr/bin/convert"
