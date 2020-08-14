be6ef355a17c4185b02238afa2ddc65a2895146c
AReallyTallDwarf <aaronverjans1@hotmail.com>
2020-08-14 14:47:22 +0200

    Pins CRUD  ability

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,60 @@
+class PinsController < ApplicationController
+    before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @pins = Pin.all.order("created_at DESC")
+    end
+
+    def show
+    end
+    
+   
+    def new
+        @pin = Pin.new
+    end
+    
+    def create
+        @pin = Pin.new(pin_params)
+        if @pin.save
+        #   flash[:success] = "Pin successfully created"
+          redirect_to @pin, notice: "Successfully created new Pin"
+        else
+        #   flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @pin.update_attributes(pin_params)
+        #   flash[:success] = "Pin was successfully updated"
+          redirect_to @pin, notice: "pin was Successfully updated!"
+        else
+        #   flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    
+    def destroy
+        if @pin.destroy
+            # flash[:success] = 'Pin was successfully deleted.'
+            redirect_to root_path, notice: "pin was Successfully deleted."
+        else
+            # flash[:error] = 'Something went wrong'
+            redirect_to pins_url
+        end
+    end
+    
+
+    private
+    def pin_params
+        params.require(:pin).permit(:title, :description)
+    end
+    
+    def find_pin
+        @pin = Pin.find(params[:id])
+    end
+    
+end
